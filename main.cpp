1952481cea86dfec8a92c224f54abc6e2b97fcec
Matthew Endsley <mendsley@gmail.com>
2016-02-10 22:11:16 -0800

    Output a 440hz wave for 10s

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+void output_sin440_10sec(tiny::audio::IRenderDevice* device, int sourceSampleRate);
+
 int main()
 {
 	if (!tiny::platformStartup())
@@ -50,5 +52,53 @@ int main()
 	}
 
 	printf("Audio device [%p]\n", device);
+	output_sin440_10sec(device, sampleRate);
 	device->release();
+}
+
+// from tiny/examples/audio_sin
+#include <vector>
+#include <time.h>
+#include <tiny/audio/resample.h>
+void output_sin440_10sec(tiny::audio::IRenderDevice* device, int sourceSampleRate)
+{
+	using namespace tiny::audio;
+
+	resample::Linear resampler(sourceSampleRate, device->sampleRate());
+	std::vector<float> scratch;
+
+	static const float frequency = 440.0f;
+	static const float twoPi = 6.283185307f;
+	const float deltaAngle = twoPi * frequency / sourceSampleRate;
+
+	if (device->start())
+	{
+		float angle = 0.0f;
+
+		time_t end = time(NULL) + 10;
+		while (time(NULL) < end)
+		{
+			float* samples;
+			int nsamples = device->acquireBuffer(&samples);
+
+			const int nrawsamples = resampler.inputSamples(nsamples);
+			scratch.resize(nrawsamples);
+			float* rawSamples = scratch.data();
+
+			for (int remaining = nrawsamples; remaining; --remaining, rawSamples += 1, angle += deltaAngle)
+			{
+				// wrap angle to [0.0, 2pi]
+				if (angle > twoPi)
+					angle -= twoPi;
+
+				const float sinAngle = sinf(angle);
+				rawSamples[0] = sinAngle;
+			}
+
+			resampler.resampleMonoToStereo(scratch.data(), nrawsamples, samples, nsamples);
+			device->commitBuffer();
+		}
+
+		device->stop();
+	}
 }
